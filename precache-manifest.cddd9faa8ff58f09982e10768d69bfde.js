a9d13b8c7a8bb3b15e7068150e33040df3f0d5ff
Jyotsana Nidhi <jnidhimay20@yahoo.com>
2020-04-21 16:09:44 +0530

    Updates

---
+++ precache-manifest.cddd9faa8ff58f09982e10768d69bfde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d614c2909b0d0523ccba04ff84a35b7e",
+    "url": "/ReactBurgerProject/index.html"
+  },
+  {
+    "revision": "0e6ed3ea556edee0ad33",
+    "url": "/ReactBurgerProject/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c894e8a8e01d6a599ce8",
+    "url": "/ReactBurgerProject/static/js/2.07a05dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactBurgerProject/static/js/2.07a05dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e6ed3ea556edee0ad33",
+    "url": "/ReactBurgerProject/static/js/main.b97bc913.chunk.js"
+  },
+  {
+    "revision": "ee20c22909315b7b3dde",
+    "url": "/ReactBurgerProject/static/js/runtime-main.f606ed7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactBurgerProject/static/media/logo.5d5d9eef.svg"
+  }
+]);
